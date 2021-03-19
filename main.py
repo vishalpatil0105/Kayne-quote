790b6f3fab477ee5ff0edc8544a0f7185d5a4469
vishal patil <80039917+vishalpatil0105@users.noreply.github.com>
2021-03-19 16:25:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
+# importing modules
 from tkinter import *
 import requests
 
-
+# Sending request to kayne website using their api 
 def get_quote():
     response = requests.get("https://api.kanye.rest")
     response.raise_for_status()
     data = response.json()
+    # Getting hold of quote
     quote = data["quote"]
+    # changing our user interface with quote 
     canvas.itemconfig(quote_text, text=quote)
 
-
+#creating window class from tk module
 window = Tk()
+# Setting parameters
 window.title("Kanye Says...")
 window.config(padx=50, pady=50)
-
+#creating canvas from tk modules to show interface
 canvas = Canvas(width=300, height=414)
 background_img = PhotoImage(file="background.png")
 canvas.create_image(150, 207, image=background_img)
